86f2dbc2c0aea4d3dfed9a65169ba19b015a8272
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-19 18:26:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,15 +6,14 @@ end
 
 def monopoly_with_second_tier
   hash = {
-    railroads: "Tobie"
+    railroads: {}
   }
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  hash = {
+    railroads: {}
+  }
 end
 
 def monopoly_with_fourth_tier
