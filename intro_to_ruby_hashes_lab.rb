5f3ac7a65bcd33f9c9a79f0e3bff09ed27adec88
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-19 18:30:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,15 +6,16 @@ end
 
 def monopoly_with_second_tier
   hash = {
-    railroads: {pieces: 4},
-    rent_in_dollars: {}
+    railroads: {pieces: 4,
+      rent_in_dollars: {}
+    }
+    
   }
 end
 
 def monopoly_with_third_tier
   hash = {
-    railroads: {pieces: 4},
-    rent_in_dollars: {}
+    railroads: {pieces: 4, rent_in_dollars: {}}
   }
 end
 
